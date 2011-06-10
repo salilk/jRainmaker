32f60281c60972b1aed589e2d0991902bef2a904
Sachin Handiekar <sach21@gmail.com>
2011-06-10 15:17:07 +0100

    Initial Commit

---
+++ src/org/jrainmaker/config/StatusCode.java
@@ -0,0 +1,19 @@
+package org.jrainmaker.config;
+
+public class StatusCode {
+
+	/** Field description */
+	private static int EMAIL_IN_SEARCH = 202;
+
+	/** Field description */
+	private static int EMAIL_NOT_FOUND = 404;
+
+	/** Field description */
+	private static int INVALID_API_KEY = 403;
+
+	/** Field description */
+	private static int INVALID_EMAIL_ADDR = 422;
+
+	/** Field description */
+	private static int REQ_SUCCESS = 200;
+}
