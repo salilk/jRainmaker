22d9739a7e115a59830fde74e957f81aa049fc6f
Sachin Handiekar <sach21@gmail.com>
2011-07-01 22:29:45 +0100

    Code Cleanup

--- src/org/jrainmaker/RainMaker.java
+++ src/org/jrainmaker/RainMaker.java
@@ -9,51 +9,48 @@ import org.jrainmaker.http.RainMakerHttpRequest;
 import java.text.MessageFormat;
 
 public class RainMaker {
+    private String apiKey;
 
-	private String apiKey;
+    public RainMaker(String apiKey) {
+        if (apiKey == null) {
+            throw new IllegalArgumentException("apiKey cannot be null.");
+        }
 
-	public RainMaker(String apiKey) {
-		if (apiKey == null) {
-			throw new IllegalArgumentException("apiKey cannot be null.");
-		}
+        if (apiKey.trim().length() == 0) {
+            throw new IllegalArgumentException("apiKey cannot be empty.");
+        }
 
-		if (apiKey.trim().length() == 0) {
-			throw new IllegalArgumentException("apiKey cannot be empty.");
-		}
+        this.apiKey = apiKey;
+    }
 
-		this.apiKey = apiKey;
-	}
+    public RainMakerEntity getPersonInformation(String email)
+            throws RainMakerException {
+        String requestParams =
+            MessageFormat.format(Constants.EMAIL_FORMAT, email) + "&"
+            + MessageFormat.format(Constants.API_KEY_FORMAT, apiKey);
+        RainMakerEntity message = null;
+        Gson gson = new Gson();
+        String response = RainMakerHttpRequest.sendRequest(requestParams);
 
-	public RainMakerEntity getPersonInformation(String email)
-			throws RainMakerException {
-		String requestParams = MessageFormat.format(Constants.EMAIL_FORMAT,
-				email)
-				+ "&"
-				+ MessageFormat.format(Constants.API_KEY_FORMAT, apiKey);
-		RainMakerEntity message = null;
-		Gson gson = new Gson();
-		String response = RainMakerHttpRequest.sendRequest(requestParams);
+        message = gson.fromJson(response, RainMakerEntity.class);
 
-		message = gson.fromJson(response, RainMakerEntity.class);
+        return message;
+    }
 
-		return message;
-	}
+    public RainMakerEntity getPersonInformation(String email,
+            int timeoutSeconds)
+            throws RainMakerException {
+        String requestParams =
+            MessageFormat.format(Constants.EMAIL_FORMAT, email) + "&"
+            + MessageFormat.format(Constants.API_KEY_FORMAT, apiKey) + "&"
+            + MessageFormat.format(Constants.TIMEOUT_SECONDS_FORMAT,
+                                   timeoutSeconds);
+        RainMakerEntity message = null;
+        Gson gson = new Gson();
+        String response = RainMakerHttpRequest.sendRequest(requestParams);
 
-	public RainMakerEntity getPersonInformation(String email, int timeoutSeconds)
-			throws RainMakerException {
-		String requestParams = MessageFormat.format(Constants.EMAIL_FORMAT,
-				email)
-				+ "&"
-				+ MessageFormat.format(Constants.API_KEY_FORMAT, apiKey)
-				+ "&"
-				+ MessageFormat.format(Constants.TIMEOUT_SECONDS_FORMAT,
-						timeoutSeconds);
-		RainMakerEntity message = null;
-		Gson gson = new Gson();
-		String response = RainMakerHttpRequest.sendRequest(requestParams);
+        message = gson.fromJson(response, RainMakerEntity.class);
 
-		message = gson.fromJson(response, RainMakerEntity.class);
-
-		return message;
-	}
+        return message;
+    }
 }
